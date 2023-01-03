99df79385cc17e4eb3bf6d135391c4acd3326739
wangwei <wei_company@126.com>
2023-01-03 19:09:32 +0800

    [timeline_widget] Refactor module: add doctest tests & reformat some code & improve function calls

--- dearbagplayer/timeline_widgets.py
+++ dearbagplayer/timeline_widgets.py
@@ -8,10 +8,55 @@ sys.path.append(script_abspath)
 try:
     import timeline
 except:
-    raise
+    raise ModuleNotFoundError('Module timeline not found.')
 
 
-class TimelineWidgets():
+class TimelineWidgets:
+    """
+    Create Timeline Wigets given Start Time and Duration
+
+    Usage examples:
+
+    # Init DearPyGui and create TimelineWidgets instance
+    >>> dpg.create_context()
+    >>> timeline_widget = TimelineWidgets()  # start_time=0, duration=0 by default
+
+    # Set up DearPyGui window
+    >>> with dpg.window(label="Timeline", tag="Primary Window"):
+    ...     timeline_widget.createWidgets()
+    >>> dpg.create_viewport(title="Timeline Test", height=400, width=600, x_pos=0, y_pos=0)
+    >>> dpg.set_primary_window("Primary Window", True)
+    >>> dpg.setup_dearpygui()
+    >>> dpg.show_viewport()
+
+    # Test properties
+    >>> timeline_widget.start = 1
+    >>> print(timeline_widget.start, timeline_widget.end, timeline_widget.duration)
+    1 1.0 0.0
+    >>> timeline_widget.duration = 5
+    >>> print(timeline_widget.start, timeline_widget.end, timeline_widget.duration)
+    1 6 5
+    >>> timeline_widget.end = 3
+    >>> print(timeline_widget.start, timeline_widget.end, timeline_widget.duration)
+    1 3 2
+    >>> timeline_widget.now()
+    1.0
+    >>> timeline_widget.play()  # default speed = 1.0
+    >>> timeline_widget.render(1.2)
+    >>> timeline_widget.now()
+    2.2
+
+    # Test widget running
+    >>> import time; start = time.time()
+    >>> while dpg.is_dearpygui_running():
+    ...     timeline_widget.render(dpg.get_delta_time())
+    ...     dpg.render_dearpygui_frame()
+    ...     if time.time() - start > 3.0:
+    ...         break
+    >>> dpg.destroy_context()
+    """
+    __slots__ = ('__timeline', 'timeline_bar', 'timeline_bar2', 'head_updated', 'is_played',
+                 'text_box', 'speed_box', 'play_button', 'pause_button', 'stop_button')
 
     def __init__(self, start_time=0.0, duration=0.0, loop_enabled=True):
         self.__timeline = timeline.Timeline(start_time, duration, loop_enabled)
@@ -56,20 +101,30 @@ class TimelineWidgets():
     def resetLimits(self, min_t, max_t):
         self.__timeline.start = min_t
         self.__timeline.end = max_t
-        dpg.configure_item(self.timeline_bar2, min_value=self.__timeline.start, max_value=self.__timeline.end)
+        dpg.configure_item(
+            self.timeline_bar2, min_value=self.__timeline.start, max_value=self.__timeline.end
+        )
 
     def createWidgets(self):
         with dpg.group(horizontal=True):
-            self.timeline_bar = dpg.add_progress_bar(label="Timeline", overlay=f"{self.__timeline.start}/{self.__timeline.end}")
+            self.timeline_bar = dpg.add_progress_bar(
+                label="Timeline", overlay=f"{self.__timeline.start}/{self.__timeline.end}"
+            )
             self.text_box = dpg.add_text(label="")
 
-        self.timeline_bar2 = dpg.add_slider_float(label="", default_value=self.__timeline.start,
-                                                  min_value=self.__timeline.start, max_value=self.__timeline.end,
-                                                  callback=self.timelineSettingCb)
+        self.timeline_bar2 = dpg.add_slider_float(
+            label="", default_value=self.__timeline.start,
+            min_value=self.__timeline.start, max_value=self.__timeline.end,
+            callback=self.timelineSettingCb
+        )
         with dpg.group(horizontal=True):
-            self.speed_box = dpg.add_drag_float(label="Speed", default_value=1.0, min_value=-5.0, max_value=5.0, width=50,
-                               callback=self.speedCb)
-            self.play_button = dpg.add_button(label="Play", arrow=True, direction=dpg.mvDir_Right, callback=self.playCb)
+            self.speed_box = dpg.add_drag_float(
+                label="Speed", default_value=1.0, min_value=-5.0, max_value=5.0,
+                width=50, callback=self.speedCb
+            )
+            self.play_button = dpg.add_button(
+                label="Play", arrow=True, direction=dpg.mvDir_Right, callback=self.playCb
+            )
             self.pause_button = dpg.add_button(label="Pause", callback=self.pauseCb)
             self.stop_button = dpg.add_button(label="Stop", callback=self.stopCb)
             dpg.disable_item(self.pause_button)
@@ -141,7 +196,54 @@ class TimelineWidgets():
         self.__timeline.render(delta_time)
         self.updateTimeline(self.__timeline.now())
 
+
 class TimelineWidgetsWithSeries(TimelineWidgets):
+    """
+    Create Timeline Wigets from Time Series
+
+    Usage examples:
+
+    # Init DearPyGui and create TimelineWidgetsWithSeries instance
+    >>> dpg.create_context()
+    >>> import numpy as np
+    >>> series = np.linspace(0.0, 10.0, 50)
+    >>> timeline_widget = TimelineWidgetsWithSeries(series)
+
+    # Set up DearPyGui window
+    >>> with dpg.window(label="Timeline-Series", tag="Primary Window 2"):
+    ...     timeline_widget.createWidgets()
+    >>> dpg.create_viewport(title="Timeline-Series Test", height=400, width=600, x_pos=0, y_pos=0)
+    >>> dpg.set_primary_window("Primary Window 2", True)
+    >>> dpg.setup_dearpygui()
+    >>> dpg.show_viewport()
+
+    # Test properties
+    >>> timeline_widget.start = 1
+    >>> print(timeline_widget.start, timeline_widget.end, timeline_widget.duration)
+    1.0 11.0 10.0
+    >>> timeline_widget.duration = 5
+    Traceback (most recent call last):
+      ...
+    AttributeError: can't set attribute
+    >>> timeline_widget.end = 3
+    >>> print(timeline_widget.start, timeline_widget.end, timeline_widget.duration)
+    -7.0 3.0 10.0
+    >>> timeline_widget.now()
+    0.0
+    >>> timeline_widget.play()  # default speed = 1.0
+    >>> timeline_widget.render(1.2)
+    >>> timeline_widget.now()
+    1.2
+
+    # Test widget running
+    >>> import time; start = time.time()
+    >>> while dpg.is_dearpygui_running():
+    ...     timeline_widget.render(dpg.get_delta_time())
+    ...     dpg.render_dearpygui_frame()
+    ...     if time.time() - start > 3.0:
+    ...         break
+    >>> dpg.destroy_context()
+    """
 
     def __init__(self, series, loop_enabled=True):
         self.__timeline = timeline.TimelineWithSeries(series, loop_enabled)
@@ -157,7 +259,10 @@ class TimelineWidgetsWithSeries(TimelineWidgets):
 
     @start.setter
     def start(self, value):
-        pass
+        self.__timeline.start = value
+        dpg.configure_item(
+            self.timeline_bar2, min_value=self.__timeline.start, max_value=self.__timeline.end
+        )
 
     @property
     def end(self):
@@ -165,63 +270,27 @@ class TimelineWidgetsWithSeries(TimelineWidgets):
 
     @end.setter
     def end(self, value):
-        pass
+        self.__timeline.end = value
+        dpg.configure_item(
+            self.timeline_bar2, min_value=self.__timeline.start, max_value=self.__timeline.end
+        )
 
     @property
     def duration(self):
+        """Override property to remove setter"""
         return self.__timeline.duration
-
-    @duration.setter
-    def duration(self, value):
-        pass
 
     @property
     def index(self):
         return self.__timeline.index
 
     def getTimestamp(self, index):
-        return self.__timeline.series[index]
+        return self.__timeline.getTimestamp(index)
 
     def getIndex(self, timestamp):
         return self.__timeline.getIndex(timestamp)
 
+
 if __name__ == "__main__":
-    dpg.create_context()
-
-    timeline1 = TimelineWidgets(duration=10.0, start_time=4.0)
-    import numpy as np
-    series = np.linspace(0.0, 20.0, 100)
-    timeline2 = TimelineWidgetsWithSeries(series)
-
-    # Call
-    print(f"{timeline1.start}, {timeline1.end}, {timeline1.duration}")
-    timeline1.start = 2.5
-    timeline1.duration = 15.0
-    print(f"{timeline1.start}, {timeline1.end}, {timeline1.duration}")
-
-    print(f"{timeline2.start}, {timeline2.end}, {timeline2.duration}")
-    timeline2.start = 2.5
-    timeline2.duration = 15.0
-    print(f"{timeline2.start}, {timeline2.end}, {timeline2.duration}")
-
-    with dpg.window(label="Timeline", tag="Primary Window"):
-        timeline1.createWidgets()
-        timeline2.createWidgets()
-
-    dpg.create_viewport(title="Timeline Test", height=400, width=600, x_pos=0, y_pos=0)
-    dpg.set_primary_window("Primary Window", True)
-
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-
-    # dpg.start_dearpygui()
-    while dpg.is_dearpygui_running():
-        # Timeline rendering
-        print("dpg.get_delta_time()=", dpg.get_delta_time())
-        timeline1.render(dpg.get_delta_time())
-        timeline2.render(dpg.get_delta_time())
-
-        # Render frame
-        dpg.render_dearpygui_frame()
-
-    dpg.destroy_context()
+    import doctest
+    doctest.testmod()
